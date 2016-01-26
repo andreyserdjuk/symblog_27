d9d561a49b99d17e13a9c1a6c1fa08a8d58b2750
andreyserdjuk <ase6120@gmail.com>
2016-01-27 00:01:48 +0200

    moved FOSUserbundle layout overriding

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new \FOSUserExtendBundle\FOSUserExtendBundle(),
             new \Blogger\BlogBundle\BloggerBlogBundle(),
             new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
