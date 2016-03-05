0509512b3e098ca28ad19fd2bf99bff2eacb3792
andreyserdjuk <ase6120@gmail.com>
2016-03-06 00:30:32 +0200

    syntax psr2 licking

--- src/Blogger/BlogBundle/Twig/Extensions/BloggerBlogExtension.php
+++ src/Blogger/BlogBundle/Twig/Extensions/BloggerBlogExtension.php
@@ -6,7 +6,9 @@ class BloggerBlogExtension extends \Twig_Extension
 {
     public function getFilters()
     {
-        return [ new \Twig_SimpleFilter('created_ago', [$this, 'createdAgo'])];
+        return [
+            new \Twig_SimpleFilter('created_ago', [$this, 'createdAgo'])
+        ];
     }
 
     /**
@@ -18,30 +20,26 @@ class BloggerBlogExtension extends \Twig_Extension
     public function createdAgo(\DateTime $dateTime)
     {
         $delta = time() - $dateTime->getTimestamp();
-        if ($delta < 0)
+
+        if ($delta < 0) {
             throw new \InvalidArgumentException("createdAgo is unable to handle dates in the future");
+        }
 
         $duration = "";
-        if ($delta < 60)
-        {
+
+        if ($delta < 60) {
             // Seconds
             $time = $delta;
             $duration = $time . " second" . (($time ===0 || $time > 1) ? "s" : "") . " ago";
-        }
-        else if ($delta < 3600)
-        {
+        } elseif ($delta < 3600) {
             // Mins
             $time = floor($delta / 60);
             $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
-        }
-        else if ($delta <= 86400)
-        {
+        } elseif ($delta <= 86400) {
             // Hours
             $time = floor($delta / 3600);
             $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
-        }
-        else
-        {
+        } else {
             // Days
             $time = floor($delta / 86400);
             $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
